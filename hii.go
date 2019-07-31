nmeum/hii

commit d3dc01ea868a9e80c24ac5412ceec547a36a224d

fmtEvent: Always strip user/channel prefix from output string
PRIVMSGs are not the only messages prefixed with a channel name. KICK
and MODE messages, for example, also have this prefix. For consistency
always strip it.

This shouldn't cause any data transparency issues since there are no
Pretty() formatted events which begin with user controlled data.

hii.go
@@ -489,10 +489,9 @@ func fmtEvent(event *girc.Event, strip bool) (string, bool) {
 		return "", false
 	}
 
-	if strip && (event.IsFromChannel() || event.IsFromUser()) {
+	if strip {
 		// Strip the user/channel name from the output string
 		// since this information is already encoded in the path.
-
 		prefix := fmt.Sprintf("[%s] ", event.Params[0])
 		out = strings.TrimPrefix(out, prefix)
 	}

commit 04350e8e7ca915ca69915365bad847b3ee5183ec

fmtEvent: Check parameter length before stripping
This fixes a bug introduced in d3dc01ea868a9e80c24ac5412ceec547a36a224d
where girc would crash if fmtEvent was called for an event without
parameters.

hii.go
@@ -485,7 +485,7 @@ func fmtEvent(event *girc.Event, strip bool) (string, bool) {
 		return "", false
 	}
 
-	if strip {
+	if strip && len(event.Params) >= 1 { // KICK, MODE, TOPIC, PRIVMSG, …
 		// Strip the user/channel name from the output string
 		// since this information is already encoded in the path.
 		prefix := fmt.Sprintf("[%s] ", event.Params[0])